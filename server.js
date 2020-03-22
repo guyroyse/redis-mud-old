7ff27418684121cbf576c38c121ccd4d5365b079
Guy Royse <guy@guyroyse.com>
2020-03-22 10:02:55 -0400

    serving up static content

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("Hello worlds!")
+const http = require('http')
+
+const StaticServer = require('node-static').Server
+
+let fileServer = new StaticServer('./static')
+let httpServer = http.createServer((request, response) => {
+  request
+    .addListener('end', () => fileServer.serve(request, response))
+    .resume()
+})
+
+httpServer.listen(8080)
