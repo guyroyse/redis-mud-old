41d4eb086215b11a4364789198a1b7455828e438
Guy Royse <guy@guyroyse.com>
2020-04-12 23:58:26 -0400

    made rename pendable

--- spec/commands/rename-command-spec.js
+++ spec/commands/rename-command-spec.js
@@ -6,26 +6,27 @@ const sinonChai = require('sinon-chai')
 
 chai.use(sinonChai)
 
-const Rename = require('../../mud').Commands.Rename
-const Room = require('../../mud').Room
+const Rename = require('../../mud/commands/rename-command')
+const Room = require('../../mud/things/room')
 
 xdescribe("Rename", function() {
 
   beforeEach(function() {
-    this.room = sinon.createStubInstance(Room)
-    this.stream = { send: sinon.spy() }
-
-    this.subject = new Rename(this.room)
+    this.subject = new Rename()
   })
 
-  it("renames the current room", function() {
-    this.subject.execute(this.stream, "/rename room The Fub")
+  context("when executed", function() {
+    beforeEach(function() {
+      this.room = sinon.createStubInstance(Room)
+      this.response = this.subject.execute("/rename room The Fub", this.room)
+    })
 
-    expect(this.room.name).to.have.been.calledWith("The Fub")
+    it("renames the current room", function() {
+      expect(this.room.name).to.have.been.calledWith("The Fub")
+    })
 
-    expect(this.stream.send).to.have.been.calledTwice
-    expect(this.stream.send.firstCall).to.have.been.calledWith("Room renamed.")
-    expect(this.stream.send.lastCall).to.have.been.calledWith("")
+    it("reports the rename", function() {
+      expect(this.response).to.equal("Room renamed.")
+    })
   })
-
 })
