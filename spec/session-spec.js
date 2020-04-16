78b56f5f4fb36ef00539f3605a118604bb2a1153
Guy Royse <guy@guyroyse.com>
2020-04-15 21:46:19 -0400

    session fully tested, if a bit of an ugly mirror

--- spec/session-spec.js
+++ spec/session-spec.js
@@ -16,6 +16,8 @@ const WebSocket = require('ws')
 describe("Session", function() {
   beforeEach(function() {
     sinon.stub(MessageProcessor.prototype, 'processMessage')
+    sinon.stub(Dungeon.prototype, 'open')
+    sinon.stub(Dungeon.prototype, 'fetchOrCreateHub')
 
     this.stubbedWebSocket = sinon.createStubInstance(WebSocket)
 
@@ -29,16 +31,11 @@ describe("Session", function() {
   context("when started", function() {
     beforeEach(function() {
       this.stubbedRoom = sinon.createStubInstance(Room)
+      this.stubbedRoom.name.returns("The Room")
 
-      sinon.stub(Dungeon.prototype, 'open')
-      sinon.stub(Dungeon.prototype, 'fetchOrCreateHub')
       Dungeon.prototype.fetchOrCreateHub.returns(this.stubbedRoom)
 
       return this.subject.start()
-    })
-
-    afterEach(function() {
-      sinon.restore()
     })
 
     it("opens the dungeon", function() {
@@ -49,12 +46,21 @@ describe("Session", function() {
       expect(Dungeon.prototype.fetchOrCreateHub).to.have.been.called
     })
 
-    it("it sends the message of the day")
-
-    it("it sends a prompt")
+    it("it sends the message of the day and the prompt", function() {
+      expect(this.stubbedWebSocket.send.firstCall)
+        .to.have.been.calledWith("Welcome to RedisMUD!")
+      expect(this.stubbedWebSocket.send.secondCall)
+        .to.have.been.calledWith("Beware. You are likely to be eaten by a grue.")
+      expect(this.stubbedWebSocket.send.thirdCall)
+        .to.have.been.calledWith("")
+      expect(this.stubbedWebSocket.send.getCall(3))
+        .to.have.been.calledWith("You are in [The Room]")
+    })
 
     context("when processing a message", function() {
       beforeEach(function() {
+        this.stubbedWebSocket.send.resetHistory()
+        MessageProcessor.prototype.processMessage.returns("some response")
         this.subject.processMessage("some message")
       })
 
@@ -68,8 +74,16 @@ describe("Session", function() {
           .to.have.been.calledWith(expectedContext, "some message")
       })
 
-      it("returns the response to the web socket")
-      it("it sends a prompt")
+      it("returns the response to the web socket", function() {
+        expect(this.stubbedWebSocket.send.firstCall)
+          .to.have.been.calledWith("")
+        expect(this.stubbedWebSocket.send.secondCall)
+          .to.have.been.calledWith("some response")
+        expect(this.stubbedWebSocket.send.thirdCall)
+          .to.have.been.calledWith("")
+        expect(this.stubbedWebSocket.send.getCall(3))
+          .to.have.been.calledWith("You are in [The Room]")
+      })
     })
 
   })
