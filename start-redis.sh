024abcbf246b39ed462ee8596b11b869cbf97621
Guy Royse <guy@guyroyse.com>
2020-05-01 11:22:45 -0400

    updated to use my Dockerfile

--- start-redis.sh
+++ start-redis.sh
@@ -1,9 +1,14 @@
-docker image pull redislabs/redismod
+docker image pull guyroyse/redismod
 
 docker run \
   -p 6379:6379 \
   -v `pwd`/data:/data \
-  redislabs/redismod \
+  guyroyse/redismod \
   --loadmodule /usr/lib/redis/modules/redisgraph.so \
+  --loadmodule /usr/lib/redis/modules/redisbloom.so \
+  --loadmodule /usr/lib/redis/modules/redisai.so \
+    ONNX redisai_onnxruntime/redisai_onnxruntime.so \
+    TF redisai_tensorflow/redisai_tensorflow.so \
+    TFLITE redisai_tflite/redisai_tflite.so \
+    TORCH redisai_torch/redisai_torch.so \
   --dir /data \
-  --save 60 5
