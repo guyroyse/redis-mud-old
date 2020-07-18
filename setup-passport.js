30e988a722484b30841e014b86698245b302fcf1
Guy Royse <guy@guyroyse.com>
2020-07-17 22:54:34 -0400

    cleaner code

--- setup-passport.js
+++ setup-passport.js
@@ -2,26 +2,37 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
 
+let ourUsers
+
 function init(app, users) {
 
-  passport.use(
-    new LocalStrategy({ usernameField: 'name', passwordField: 'password' },
-    async (name, password, done) => {
-      let user = users.find(user => user.name === name)
-      if (!user) return done(null, false, { message: "Invalid username"})
-      let success = await bcrypt.compare(password, user.hashedPassword)
-      if (success) {
-        return done(null, user)
-      } else {
-        return done(null, false, { message: "Invalid password" })
-      }
-    }))
+  ourUsers = users
 
-  passport.serializeUser((user, done) => done(null, user.name))
-  passport.deserializeUser((id, done) => done(null, users.find(user => user.name === id)))
+  let strategy = new LocalStrategy({ usernameField: 'name', passwordField: 'password' }, authenticate)
+  passport.use(strategy)
+  passport.serializeUser(serializeUser)
+  passport.deserializeUser(deserializeUser)
 
   app.use(passport.initialize())
   app.use(passport.session())
+}
+
+async function authenticate(name, password, done) {
+  let user = ourUsers.find(user => user.name === name)
+  if (!user) return done(null, false, { message: "Invalid username"})
+
+  let success = await bcrypt.compare(password, user.hashedPassword)
+  if (!success) return done(null, false, { message: "Invalid password" })
+  
+  return done(null, user)
+}
+
+function serializeUser(user, done) {
+  done(null, user.name);
+}
+
+function deserializeUser(id, done) {
+  done(null, ourUsers.find(user => user.name === id))
 }
 
 module.exports = init
